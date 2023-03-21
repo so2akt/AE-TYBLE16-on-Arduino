563089fe6f6ffd852e9401331d78ea89b58ea95a
so2akt <17232984+so2akt@users.noreply.github.com>
2023-03-21 20:24:23 +0900

    add an "feature" on LED working w/ illumi sensor: if illumi sensor detected dark, LED is ON. note that the threshold is observed value

--- door-sensor/door-sensor.ino
+++ door-sensor/door-sensor.ino
@@ -1,11 +1,22 @@
-#define WORK_IND_LED  6   /* P0.06 (DIP10) */
+#define WORK_IND_LED  (6)   /* DIP10 (P0.06) */
+#define ILLUMI_IN     (5)   /* DIP9 (P0.05, AIN6) */
+#define AD_CONV_RES   (8)   /* 8-bit: 0 - 255 */
+
+#define ILLUMI_DARK_THRESH  (10)  /* note: observed value */
 
 void setup() {
+  /* pin direction settings */
+  pinMode(ILLUMI_IN, INPUT);
   pinMode(WORK_IND_LED, OUTPUT);
+
+  /* A/D converter settings */
+  analogReference(AR_DEFAULT);  /* range: 0[V] - VDD */
+  analogReadResolution(AD_CONV_RES);
+
+  /* output initial settings */
   digitalWrite(WORK_IND_LED, LOW);
 }
 
 void loop() {
-  digitalWrite(WORK_IND_LED, LOW == digitalRead(WORK_IND_LED) ? HIGH : LOW);
-  delay(500);
+  digitalWrite(WORK_IND_LED, ILLUMI_DARK_THRESH >= analogRead(ILLUMI_IN) ? HIGH : LOW);
 }
